d1a7fd1490075eaf10c46808b034491f2f5c3899
Shehan Disanayake <dmcshehan@gmail.com>
2018-12-28 18:12:46 +0530

    Updates

---
+++ precache-manifest.80338dad32b3a129e9bf3f5e6d7d94f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c8aa453299f7a6200fe",
+    "url": "/random-quotes/static/css/main.5a6ef364.chunk.css"
+  },
+  {
+    "revision": "0c8aa453299f7a6200fe",
+    "url": "/random-quotes/static/js/main.0c8aa453.chunk.js"
+  },
+  {
+    "revision": "6c37a62dc2be6fd38ddd",
+    "url": "/random-quotes/static/js/1.6c37a62d.chunk.js"
+  },
+  {
+    "revision": "a4554ae4c421f369a667",
+    "url": "/random-quotes/static/js/runtime~main.a4554ae4.js"
+  },
+  {
+    "revision": "118ee064ff9e2bdb8e88a17367088713",
+    "url": "/random-quotes/static/media/road.118ee064.jpeg"
+  },
+  {
+    "revision": "5df8b39f7ac25c143490defe2864224a",
+    "url": "/random-quotes/index.html"
+  }
+];
